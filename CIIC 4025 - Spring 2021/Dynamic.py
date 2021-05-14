6983f5ee4cea7179212df66d73255a1da5797824
Alondra Pereira <alondra.pereira@upr.edu>
2021-05-13 20:39:25 -0400

    Rod Cutting, Change Making and Activity Selection

--- CIIC 4025 - Spring 2021/Dynamic.py
+++ CIIC 4025 - Spring 2021/Dynamic.py
@@ -97,12 +97,74 @@ def knapsack_01(W, wt, val, n):
                 A[i][w] = A[i - 1][w]
     return A[n][W]
 
+
 # *************** Rod Cutting *************** #
+# A Dynamic Programming solution for Rod cutting problem
+
+
+# Returns the best obtainable price for a rod of length n and
+# price[] as prices of different pieces
+def cut_rode(price, n):
+    min = -float("inf")
+    val = [0 for x in range(n + 1)]
+    val[0] = 0
+    # Build the table val[] in bottom up manner and return
+    # the last entry from the table
+    for i in range(1, n + 1):
+        max_val = min
+        for j in range(i):
+            max_val = max(max_val, price[j] + val[i - j - 1])
+        val[i] = max_val
+
+    return val[n]
 
 # *************** Change Making *************** #
+''' We have input values of N and an array Coins
+that holds all of the coins. We use data type
+of because long we want to be able to test
+large values
+without integer overflow'''
+def get_number_of_ways(n, Coins):
+    # Create the ways array to 1 plus the amount
+    # to stop overflow
+    ways = [0] * (n + 1);
+    # Set the first way to 1 because its 0 and
+    # there is 1 way to make 0 with 0 coins
+    ways[0] = 1;
+    # Go through all of the coins
+    for i in range(len(Coins)):
+        # Make a comparison to each index value
+        # of ways with the coin value.
+        for j in range(len(ways)):
+            if (Coins[i] <= j):
+                # Update the ways array
+                ways[j] += ways[(int)(j - Coins[i])];
+    # return the value at the Nth position
+    # of the ways array.
+    return ways[n];
 
 # *************** Activity Selection *************** #
+# Returns the maximum count of non-conflicting jobs that can be performed
+# by a single person
+def activity_selection(jobs):
+    # Sort the jobs according to increasing order of their start time
+    jobs.sort(key=lambda x: x[0])
 
+    # `L[i]` stores the maximum count of non-conflicting jobs ending at i'th job
+    L = [0] * len(jobs)
+
+    for i in range(len(jobs)):
+        # consider each `j` less than `i`
+        for j in range(i):
+            # `L[i] = max(L[j])`, where `jobs[j].finish` is less than `jobs[i].start`
+            if jobs[j][1] < jobs[i][0] and L[i] < L[j]:
+                L[i] = L[j]
+
+        # increment `L[i]` since it ends at the i'th job
+        L[i] = L[i] + 1
+
+    # return the maximum job length in the list
+    return max(L)
 
 if __name__ == '__main__':
     # Input values
@@ -116,10 +178,15 @@ if __name__ == '__main__':
         'x': {'t': -2}
     }
     # Knapsack 0-1:
-    val = [3, 2, 4, 4]
+    knap_value = [3, 2, 4, 4]
     wt = [4, 3, 2, 4]
     W = 6
-    n = len(val)
+    n = len(knap_value)
+
+    # Rod Cutting
+    # Driver program to test above functions
+    arr = [1, 5, 8, 9, 10, 17, 17, 20]
+    size = len(arr)
 
     print("Bellman-Ford result: ", get_distances(example_graph, 's'))
     print("Fibonacci result: ", fibonacci_number(9))
@@ -127,4 +194,10 @@ if __name__ == '__main__':
     print("Minimum Steps Required to convert w1 to w2 : ", min_distance('carat', 'casis'))
     print("Longest Common Subsequence: ", lcs('bacad', 'accbadcb'))
     print("Sequence Analysis Result: ", sequence_alignment('AGCGCT', 'AGCCA'))
-    print("Knapsack 0-1 Optimal Solution: ", knapsack_01(W, wt, val, n))
+    print("Knapsack 0-1 Optimal Solution: ", knapsack_01(W, wt, knap_value, n))
+    print("Rod Cutting Maximum Obtainable Value: ", str(cut_rode(arr, size)))
+    print("Change Making: ", get_number_of_ways(12, [1, 5, 10]))
+    print("Activity Selection: ", activity_selection([
+        (1, 4), (3, 5), (0, 6), (5, 7), (3, 8), (5, 9),
+        (6, 10), (8, 11), (8, 12), (2, 13), (12, 14)
+    ]))
